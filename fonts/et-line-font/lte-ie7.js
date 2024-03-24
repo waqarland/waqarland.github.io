3c20424034ad0edaecc9be3dd1ac759df50fe5eb
waqarland <waqar.mahmood0987@gmail.com>
2024-03-24 22:41:21 +0500

    first commit

---
+++ fonts/et-line-font/lte-ie7.js
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="zxx">
+
+<head>
+    <title>Gigaland - NFT Marketplace Website Template</title>
+    <link rel="icon" href="images/icon-red.png" type="image/gif" sizes="16x16">
+    <meta content="text/html;charset=utf-8" http-equiv="Content-Type">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport" />
+    <meta content="Gigaland - NFT Marketplace Website Template" name="description" />
+    <meta content="" name="keywords" />
+    <meta content="" name="author" />
+    <!-- CSS Files
+    ================================================== -->
+    <link id="bootstrap" href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="css/plugins.css" rel="stylesheet" type="text/css" />    
+    <link href="css/style.css" rel="stylesheet" type="text/css" />
+    <link href="css/de-grey.css" rel="stylesheet" type="text/css" />
+    <!-- color scheme -->
+    <link id="colors" href="css/colors/scheme-04.css" rel="stylesheet" type="text/css" />
+    <link href="css/coloring.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body class="dark-scheme de-grey">
+    <div id="wrapper">
+
+            <!-- header begin -->
+            <header class="transparent">
+                <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="de-flex sm-pt10">
+                            <div class="de-flex-col">
+                                <div class="de-flex-col">
+                                    <!-- logo begin -->
+                                    <div id="logo">
+                                        <a href="03_grey-index.html">
+                                            <img alt="" src="images/logo-3.png" />
+                                        </a>
+                                    </div>
+                                    <!-- logo close -->
+                                </div>
+                                <div class="de-flex-col">
+                                    <input id="quick_search" class="xs-hide" name="quick_search" placeholder="search item here..." type="text" />
+                                </div>
+                            </div>
+                            <div class="de-flex-col header-col-mid">
+                                <!-- mainmenu begin -->
+                                <ul id="mainmenu">
+                                    <li>
+                                        <a href="03_grey-index.html">Home<span></span></a>
+                                        <ul>
+                                            <li><a href="03_grey-index.html">Homepage 1</a></li>
+                                            <li><a href="03_grey-index-2.html">Homepage 2</a></li>
+                                            <li><a href="03_grey-index-3.html">Homepage 3</a></li>
+                                            <li><a href="03_grey-index-4.html">Homepage 4</a></li>
+                                        </ul>
+                                    </li>
+                                    <li>
+                                        <a href="03_grey-explore.html">Explore<span></span></a>
+                                        <ul>
+                                            <li><a href="03_grey-explore.html">Explore</a></li>
+                                            <li><a href="03_grey-explore-2.html">Explore 2</a></li>
+                                            <li><a href="03_grey-explore-music-nft.html">New: Music NFT</a></li>
+                                            <li><a href="03_grey-collection.html">Collections</a></li>
+                                            <li><a href="03_grey-live-auction.html">Live Auction</a></li>
+                
